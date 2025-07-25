bbe83116b78935f0fede1be2a8fe3a29ece2c3b5
kbuss26 <144736945+kbuss26@users.noreply.github.com>
2025-07-25 13:28:32 -0600

    Add puzzle prototype, update dates
    As I push these updates I'm learning that I cannot keep track of dates. Might remove them in a future push if they get to be too much of a hassle

--- JavaScript/puzzle.js
+++ JavaScript/puzzle.js
@@ -1,7 +1,96 @@
 /* 
 Keifer Buss
-Last modified: 19 Jul 2025
-Sources: [2], [3], [4]
+Last modified: 23 Jul 2025
+Sources: [3], [4], [5], [6]
 */
 
-/* Under construction */ 
+let darkCount
+let darkArray = [];
+let currentWidth = 5;
+let currentHeight = 5;
+let currentId = 0;
+let currentHTML = "";
+
+// Defining the custom element for HTML
+class LightPuzzle extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        for (let i = 0; i < currentHeight; i++) {
+            this.innerHTML += "<div class='row'>"
+            for (let j = 0; j < currentWidth; j++) {
+                currentId = "piece" + (currentWidth * i + j);
+                darkArray.push(0);
+                this.innerHTML += "<button class='puzzle' id='" + currentId + "'></button>";
+            }
+            this.innerHTML += "</div>"
+        }
+    }
+}
+customElements.define("create-puzzle", LightPuzzle)
+
+// wait for page to load before loading scripts [5]
+window.onload = init;
+
+// Loaded script
+function init() {
+    const btns = document.getElementsByClassName("puzzle");
+    //console.log(btns);
+
+    // All created tiles need a click function
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            // Find the appropriate neighbors
+            let pieceArray = getNeighbors(i, currentWidth, currentHeight);
+
+            // Toggle lights on found neighbors
+            for (let n in pieceArray) {
+                //console.log(pieceArray[n])
+                if (darkArray[pieceArray[n]] === 0) {
+                    darkArray[pieceArray[n]] = 1;
+                    btns[pieceArray[n]].style.backgroundColor = "black";
+                } else {
+                    darkArray[pieceArray[n]] = 0;
+                    btns[pieceArray[n]].style.backgroundColor = "white";
+                }
+            }
+        })
+    }
+}
+
+// Get the applicable neighbors for a given tile. Width and height are assumed to be different
+// Position is also included because the puzzle must also light the selected tile
+function getNeighbors(pos, puzzleWidth, puzzleHeight) {
+    let neighbors = [pos];
+
+    // Left check
+    if ((pos % puzzleWidth) - 1 >= 0) {
+        neighbors.push(pos - 1);
+    }
+    
+    // Right check
+    if ((pos % puzzleWidth) + 1 < puzzleWidth) {
+        neighbors.push(pos + 1);
+    }
+
+    // Up check
+    if (pos - puzzleWidth >= 0) {
+        neighbors.push(pos - puzzleWidth);
+    }
+
+    // Down check
+    if (pos + puzzleWidth < puzzleWidth * puzzleHeight) {
+        neighbors.push(pos + puzzleWidth);
+    }
+
+    return neighbors;
+}
+
+// Initial function testing
+console.log(getNeighbors(6, 4, 4));
+console.log(getNeighbors(7, 4, 4));
+console.log(getNeighbors(2, 4, 4));
+console.log(getNeighbors(0, 4, 4));
+console.log(getNeighbors(13, 4, 4));
+console.log(getNeighbors(0, 1, 1));
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # kbuss26.github.io
 A basic website establishing who I am and what I do. As I develop this website from scratch, I will use documentation and tutorials and source them below.
 
+*Note:* To avoid confusion and constant file updating, additional references will be placed at the bottom of the list chronologically before occasional alphabetization updates.
+
 ## References
 [1] "How to position a div at specific coordinates." GeeksforGeeks.org. Accessed: Jul. 19, 2025. [Online.] Available: https://www.geeksforgeeks.org/javascript/how-to-position-a-div-at-specific-coordinates/
 
@@ -9,3 +11,7 @@ A basic website establishing who I am and what I do. As I develop this website f
 [3] Mozilla, 2025, "MDN Web Docs," Mozilla Foundation. [Online.] Available: https://developer.mozilla.org/en-US/.
 
 [4] W3Schools, 2025, "W3Schools Online Web Tutorials," Refsnes Data AS. [Online.] Available: https://www.w3schools.com/tags/default.asp.
+
+[5] "getElementById returns Null? [Closed]." stackoverflow.com. Accessed: Jul. 23, 2025. [Online.] Available: https://stackoverflow.com/questions/8739605/getelementbyid-returns-null
+
+[6] "JavaScript click event listener on class." stackoverflow.com. Accessed: Jul. 23, 2025. [Online.] Available. https://stackoverflow.com/questions/19655189/javascript-click-event-listener-on-class
